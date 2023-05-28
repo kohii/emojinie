2868a601b9cebef183ca8e74c815536f8a480de8
kohii <kohii.tokyo@gmail.com>
2023-05-28 17:57:54 +0900

    feat: open settings window on setup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -60,6 +60,7 @@ fn main() {
 
             let window = app.get_window("main").unwrap();
             window.set_transparent_titlebar(true, true);
+            settings::open_settings_window(&app.app_handle());
             Ok(())
         })
         .run(tauri::generate_context!())
